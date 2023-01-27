e7912d03216e681c119532c47905fbd70f67824a
salomon.cjl <contrerassalomon6@gmail.com>
2023-01-27 00:56:53 -0300

    Primer subida de task

---
+++ probando.js
@@ -0,0 +1,9 @@
+
+
+function sumaLongitudes(unString, otroString){
+    //                        ▲           ▲
+    //                  "aprendiendo" "programación"
+    return unString.length + otroString.length;  
+    //            ▲                  ▲
+    //    "aprendiendo".length  "programación".length
+  }
