3d35a04fa07caf60e2e5e74f2c0928be9c40093b
salomon.cjl <contrerassalomon6@gmail.com>
2023-02-02 04:59:42 -0300

    2-2-2023 task 2 completada

---
+++ assets/javascript/past.js
@@ -0,0 +1,20 @@
+const padrecards = document.getElementById("cards")
+let inforcards = data.events;
+
+for (let infocard of inforcards){
+    if(infocard.date < data.currentDate){
+    padrecards.innerHTML +=`<div class="card" style="width: 18rem;">
+    <img src="${infocard.image}" class="card-img-top" style="height: -webkit-fill-available;" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${infocard.name}</h5>
+    <p class="card-text">${infocard.description}</p>
+    <div class="precio-details">
+    <p class="precio"> Price: $ ${infocard.price}</p>
+    <a href="details.html" class="btn btn-primary">Details</a>
+    </div>
+    </div>
+    </div>
+    `
+}
+}
+//<>
